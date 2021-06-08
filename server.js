76899b43ec49880ead2246f86fc312261adbf347
Shohei <shohei.shibata.dev@gmail.com>
2021-06-08 09:32:23 +0000

    Completed challenge

--- server.js
+++ server.js
@@ -21,8 +21,15 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  const ipAddress = req.ip
+  const language = req.get('Accept-Language')
+  const software = req.get('User-Agent')
+
+  res.json({
+    ipaddress: ipAddress,
+    language: language,
+    software: software});
 });
 
 
